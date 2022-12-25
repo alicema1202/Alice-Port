68beb18bca691d5822eb464a1b8ddc0e6e800dea
alicema1202 <64507532+alicema1202@users.noreply.github.com>
2022-12-25 14:31:07 -0800

    scroll animations

---
+++ slide.js
@@ -0,0 +1,17 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+const hiddenElements2 = document.querySelectorAll('.hidden-delay');
+hiddenElements2.forEach((el) => observer.observe(el));
