dcc78ffae459ef4a519f28c169f96df0370b77f7
alicema1202 <64507532+alicema1202@users.noreply.github.com>
2025-01-11 00:01:55 -0800

    add back to top button

--- js/header-bar.js
+++ js/header-bar.js
@@ -1,33 +1,38 @@
 // document.querySelector(".header-bar").classList.add('animate');
 var prevScrollpos = window.pageYOffset;
-        
-        /* Get the header element and it's position */
-        var headerDiv = document.querySelector(".header-bar");
-        var headerBottom = headerDiv.offsetTop + headerDiv.offsetHeight;
 
-        window.onscroll = function() {
-        var currentScrollPos = window.pageYOffset;               
-            /* if we're scrolling up, or we haven't passed the header,
-                show the header at the top */
-        if (currentScrollPos <= 200) {
-            headerDiv.classList.remove("midpage");
-        }
-        else if (prevScrollpos > currentScrollPos) {  
-                headerDiv.style.top = "0";
-                headerDiv.classList.add("midpage");
-        }
-        else if (currentScrollPos <  200) {
-            headerDiv.style.top = "0";
-            // headerDiv.style.opacity = "0";
-            // headerDiv.classList.add("midpage");
-        
-        }
-        else if (document.querySelector(".nav-content").classList.contains('active') == false){
-            /* otherwise we're scrolling down & have passed the header so hide it */
-            headerDiv.style.top = "-200px";
-        } 
+/* Get the header element and it's position */
+var headerDiv = document.querySelector(".header-bar");
+var headerBottom = headerDiv.offsetTop + headerDiv.offsetHeight;
 
-            prevScrollpos = currentScrollPos;
-        }
-        var st = $(this).scrollTop();
-    
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;               
+    /* if we're scrolling up, or we haven't passed the header,
+        show the header at the top */
+if (currentScrollPos <= 200) {
+    headerDiv.classList.remove("midpage");
+    document.querySelector(".back-to-top").classList.add('hide');
+}
+else if (prevScrollpos > currentScrollPos) {  
+        headerDiv.style.top = "0";
+        headerDiv.classList.add("midpage");
+}
+else if (currentScrollPos <  200) {
+    headerDiv.style.top = "0";
+    // headerDiv.style.opacity = "0";
+    // headerDiv.classList.add("midpage");
+    document.querySelector(".back-to-top").classList.add('hide');
+}
+else if (document.querySelector(".nav-content").classList.contains('active') == false){
+    /* otherwise we're scrolling down & have passed the header so hide it */
+    headerDiv.style.top = "-200px";
+    document.querySelector(".back-to-top").classList.remove('hide');
+} 
+
+    prevScrollpos = currentScrollPos;
+}
+var st = $(this).scrollTop();
+
+function scrollWin() {
+    window.scrollTo(0, 0);
+}
